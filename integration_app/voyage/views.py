73c0b7da2670fc1b77f99c23ca1719a8ce658aa8
Benjam <benjamin.pelletier4gmail.com>
2025-01-27 22:25:33 +0100

    code pour l'api open-meteo

--- integration_app/voyage/views.py
+++ integration_app/voyage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
 
 # Importation des modèles
 from .models import Airport
@@ -7,21 +8,32 @@ from .models import Airport
 from .api.openMeteo import twoWeakWeatherForecast, calculGeneralWeather
 
 def acceuil(request):
+    # Normalement disponible via la requête
     countryChoosen = "France"
-    best_weather = 1000
-    weather_city = 0
 
-    country = Airport.objects.filter(country=countryChoosen).values_list('city', flat=True)
+    # Récupérer les villes du pays sélectionné
+    cities_country_chosen = Airport.objects.filter(country=countryChoosen)
     
-    #for i in range(0, len(country)) :
-        #df = twoWeakWeatherForecast(country[i].latitude, country[i].longitude)
-        #weather_city = calculGeneralWeather(df)
+    L_cities_score = []
 
-        #if(weather_city < best_weather) :
-            #best_weather = weather_city
-            #best_city = country[i].
+    # Déterminer les trois villes avec le meilleur temps sur un horizon de deux semaines
+    for c in cities_country_chosen:
+        weather = twoWeakWeatherForecast(c.latitude, c.longitude)    # Météo sur deux semaines
+        score_weather = calculGeneralWeather(weather)                # Temps général sur les deux semaines
+
+        # Eviter d'avoir trois fois la même ville
+        if not any(c.city in cle_valeur for cle_valeur in L_cities_score) :
+            if (len(L_cities_score) < 3) :
+                L_cities_score.append({c.city : score_weather})
+                L_cities_score.sort(key=lambda x : list(x.values())[0])
+            else :
+                for j in range(2, -1, -1) :
+                    if (score_weather < list(L_cities_score[j].values())[0]) :
+                        L_cities_score[j] = {c.city : score_weather}
+                        L_cities_score.sort(key=lambda x : list(x.values())[0])
+                        break                           
 
         
-    print(country)
+    print(L_cities_score)
 
     return render(request, "pageAcceuil.html")
